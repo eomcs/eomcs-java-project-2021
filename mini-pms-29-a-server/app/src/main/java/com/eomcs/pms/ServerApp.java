ce3b33e8dc667b78abe9cc6634970ec03340211b
eomjinyoung <jinyoung.eom@gmail.com>
2021-03-09 11:58:21 +0900

    29-a

---
+++ mini-pms-29-a-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -0,0 +1,8 @@
+package com.eomcs.pms;
+
+// 데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
+public class ServerApp {
+  public static void main(String[] args) {
+    System.out.println("파일 관리 서버");
+  }
+}
---
+++ mini-pms-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -0,0 +1,7 @@
+package com.eomcs.pms;
+
+public class ClientApp {
+  public static void main(String[] args) {
+    System.out.println("미니 프로젝트 관리 시스템");
+  }
+}
