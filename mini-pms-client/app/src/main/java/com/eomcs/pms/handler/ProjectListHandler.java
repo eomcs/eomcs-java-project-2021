68bc051c6a452b1a01999de900c6f9a4086bd7a8
eomjinyoung <jinyoung.eom@gmail.com>
2021-03-26 16:04:57 +0900

    30-c

--- mini-pms-client/app/src/main/java/com/eomcs/pms/handler/ProjectListHandler.java
+++ mini-pms-client/app/src/main/java/com/eomcs/pms/handler/ProjectListHandler.java
@@ -24,15 +24,37 @@ public class ProjectListHandler implements Command {
                 + "  from pms_project p"
                 + "    inner join pms_member m on p.owner=m.no"
                 + "  order by title asc");
+        PreparedStatement stmt2 = con.prepareStatement(
+            "select" 
+                + "    m.no,"
+                + "    m.name"
+                + " from pms_member_project mp"
+                + "     inner join pms_member m on mp.member_no=m.no"
+                + " where "
+                + "     mp.project_no=?");
         ResultSet rs = stmt.executeQuery()) {
 
       while (rs.next()) {
-        System.out.printf("%d, %s, %s, %s, %s\n", 
+        // 1) 프로젝트의 팀원 목록 가져오기
+        stmt2.setInt(1, rs.getInt("no"));
+        String members = "";
+        try (ResultSet memberRs = stmt2.executeQuery()) {
+          while (memberRs.next()) {
+            if (members.length() > 0) {
+              members += "/";
+            }
+            members += memberRs.getString("name");
+          }
+        }
+
+        // 2) 프로젝트 정보를 출력
+        System.out.printf("%d, %s, %s, %s, %s, [%s]\n", 
             rs.getInt("no"), 
             rs.getString("title"), 
             rs.getDate("sdt"),
             rs.getDate("edt"),
-            rs.getString("owner_name"));
+            rs.getString("owner_name"),
+            members);
       }
     }
   }
