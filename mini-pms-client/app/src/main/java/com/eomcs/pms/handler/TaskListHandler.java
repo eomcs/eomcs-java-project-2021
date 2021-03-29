29f8bc8ad0b221bda04d2543c20bb327cc465e58
eomjinyoung <jinyoung.eom@gmail.com>
2021-03-29 17:02:59 +0900

    30-d

--- mini-pms-client/app/src/main/java/com/eomcs/pms/handler/TaskListHandler.java
+++ mini-pms-client/app/src/main/java/com/eomcs/pms/handler/TaskListHandler.java
@@ -13,61 +13,59 @@ public class TaskListHandler implements Command {
   public void service() throws Exception {
     System.out.println("[작업 목록]");
 
-    String input = Prompt.inputString("프로젝트 번호? ");
+    String input = Prompt.inputString("프로젝트 번호?(전체: 빈 문자열 또는 0) ");
 
     // 1) 사용자가 입력한 문자열을 프로젝트 번호로 바꾼다.
     int projectNo = 0;
     try {
-      projectNo = Integer.parseInt(input);
+      if (input.length() != 0) {
+        projectNo = Integer.parseInt(input);
+      }
     }catch (Exception e) {
       System.out.println("프로젝트 번호를 입력하세요.");
       return;
     }
 
-    // 2) 프로젝트 정보를 가져온다.
-    String projectTitle = null;
+    // 2) 해당 프로젝트에 소속된 작업 목록을 가져온다.
     try (Connection con = DriverManager.getConnection(
         "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
-        PreparedStatement stmt = con.prepareStatement(
-            "select no,title from pms_project where no=?")) {   
+        PreparedStatement stmt2 = con.prepareStatement(
+            "select "
+                + "   t.no,"
+                + "   t.content,"
+                + "   t.deadline,"
+                + "   t.status,"
+                + "   m.no as owner_no,"
+                + "   m.name as owner_name,"
+                + "   p.no as project_no,"
+                + "   p.title as project_title"
+                + " from pms_task t "
+                + "   inner join pms_member m on t.owner=m.no"
+                + "   inner join pms_project p on t.project_no=p.no"
+                + " where "
+                + "   t.project_no=? or 0=?"
+                + " order by p.no desc, t.content asc")) {
 
-      stmt.setInt(1, projectNo);
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (!rs.next()) {
-          System.out.println("해당 프로젝트가 존재하지 않습니다.");
-          return;
-        }
-        projectTitle = rs.getString("title");
-      }
+      stmt2.setInt(1, projectNo);
+      stmt2.setInt(2, projectNo);
 
-      // 3) 해당 프로젝트에 소속된 작업 목록을 가져온다.
-      try (
-          PreparedStatement stmt2 = con.prepareStatement(
-              "select "
-                  + "   t.no,"
-                  + "   t.content,"
-                  + "   t.deadline,"
-                  + "   t.status,"
-                  + "   m.no as owner_no,"
-                  + "   m.name as owner_name"
-                  + " from pms_task t "
-                  + "   inner join pms_member m on t.owner=m.no"
-                  + " where "
-                  + "   t.project_no=?"
-                  + " order by t.content asc")) {
-
-        stmt2.setInt(1, projectNo);
-
-        try (ResultSet rs = stmt2.executeQuery()) {
-          System.out.printf("'%s' 작업 목록: \n", projectTitle);
-          while (rs.next()) {
-            System.out.printf("%d, %s, %s, %s, %s\n", 
-                rs.getInt("no"), 
-                rs.getString("content"), 
-                rs.getDate("deadline"),
-                rs.getString("owner_name"),
-                Task.getStatusLabel(rs.getInt("status")));
+      try (ResultSet rs = stmt2.executeQuery()) {
+        int count = 0;
+        while (rs.next()) {
+          if (projectNo != rs.getInt("project_no")) {
+            System.out.printf("'%s' 작업 목록: \n", rs.getString("project_title"));
+            projectNo = rs.getInt("project_no");
           }
+          System.out.printf("%d, %s, %s, %s, %s\n", 
+              rs.getInt("no"), 
+              rs.getString("content"), 
+              rs.getDate("deadline"),
+              rs.getString("owner_name"),
+              Task.getStatusLabel(rs.getInt("status")));
+          count++;
+        }
+        if (count == 0) {
+          System.out.println("해당 번호의 프로젝트가 없거나 또는 등록된 작업이 없습니다.");
         }
       }
     }
