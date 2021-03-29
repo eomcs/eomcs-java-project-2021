2b4238ad3dcab14d6f0062b8aa43df7b6af299f3
eomjinyoung <jinyoung.eom@gmail.com>
2021-03-29 15:59:17 +0900

    30-d

--- mini-pms-client/app/src/main/java/com/eomcs/pms/handler/TaskListHandler.java
+++ mini-pms-client/app/src/main/java/com/eomcs/pms/handler/TaskListHandler.java
@@ -15,6 +15,7 @@ public class TaskListHandler implements Command {
 
     String input = Prompt.inputString("프로젝트 번호? ");
 
+    // 1) 사용자가 입력한 문자열을 프로젝트 번호로 바꾼다.
     int projectNo = 0;
     try {
       projectNo = Integer.parseInt(input);
@@ -23,6 +24,8 @@ public class TaskListHandler implements Command {
       return;
     }
 
+    // 2) 프로젝트 정보를 가져온다.
+    String projectTitle = null;
     try (Connection con = DriverManager.getConnection(
         "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
         PreparedStatement stmt = con.prepareStatement(
@@ -34,29 +37,37 @@ public class TaskListHandler implements Command {
           System.out.println("해당 프로젝트가 존재하지 않습니다.");
           return;
         }
+        projectTitle = rs.getString("title");
       }
 
+      // 3) 해당 프로젝트에 소속된 작업 목록을 가져온다.
       try (
           PreparedStatement stmt2 = con.prepareStatement(
               "select "
-                  + " t.no,"
-                  + " t.content,"
-                  + " t.deadline,"
-                  + " t.status,"
-                  + " m.no as owner_no,"
-                  + " m.name as owner_name"
+                  + "   t.no,"
+                  + "   t.content,"
+                  + "   t.deadline,"
+                  + "   t.status,"
+                  + "   m.no as owner_no,"
+                  + "   m.name as owner_name"
                   + " from pms_task t "
-                  + " inner join pms_member m on t.owner=m.no"
-                  + " order by t.content asc");
-          ResultSet rs = stmt2.executeQuery()) {
+                  + "   inner join pms_member m on t.owner=m.no"
+                  + " where "
+                  + "   t.project_no=?"
+                  + " order by t.content asc")) {
 
-        while (rs.next()) {
-          System.out.printf("%d, %s, %s, %s, %s\n", 
-              rs.getInt("no"), 
-              rs.getString("content"), 
-              rs.getDate("deadline"),
-              rs.getString("owner_name"),
-              Task.getStatusLabel(rs.getInt("status")));
+        stmt2.setInt(1, projectNo);
+
+        try (ResultSet rs = stmt2.executeQuery()) {
+          System.out.printf("'%s' 작업 목록: \n", projectTitle);
+          while (rs.next()) {
+            System.out.printf("%d, %s, %s, %s, %s\n", 
+                rs.getInt("no"), 
+                rs.getString("content"), 
+                rs.getDate("deadline"),
+                rs.getString("owner_name"),
+                Task.getStatusLabel(rs.getInt("status")));
+          }
         }
       }
     }
