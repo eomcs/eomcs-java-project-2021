8e3a378bcb1fc3e670fbd099f7bd07658057ddff
eomjinyoung <jinyoung.eom@gmail.com>
2021-05-06 14:18:20 +0900

    .

---
+++ docs/data-v3.sql
@@ -0,0 +1,23 @@
+-- pms_member 테이블 예제 데이터 삽입
+insert into pms_member(no, name, email, password)  values(1, 'aaa', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(2, 'bbb', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(3, 'ccc', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(4, 'ddd', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(5, 'eee', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(6, 'fff', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(7, 'ggg', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(8, 'hhh', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(9, 'iii', 'aaa@test.com', password('1111'));
+insert into pms_member(no, name, email, password)  values(10, 'jjj', 'aaa@test.com', password('1111'));
+
+-- pms_board 테이블 예제 데이터 삽입
+insert into pms_board(no, title, content, writer) values(11, '제목101', '내용101', 1);
+insert into pms_board(no, title, content, writer) values(12, '제목101', '내용101', 1);
+insert into pms_board(no, title, content, writer) values(13, '제목101', '내용101', 1);
+insert into pms_board(no, title, content, writer) values(14, '제목101', '내용101', 2);
+insert into pms_board(no, title, content, writer) values(15, '제목101', '내용101', 2);
+insert into pms_board(no, title, content, writer) values(16, '제목101', '내용101', 4);
+insert into pms_board(no, title, content, writer) values(17, '제목101', '내용101', 4);
+insert into pms_board(no, title, content, writer) values(18, '제목101', '내용101', 7);
+insert into pms_board(no, title, content, writer) values(19, '제목101', '내용101', 7);
+insert into pms_board(no, title, content, writer) values(20, '제목101', '내용101', 9);
